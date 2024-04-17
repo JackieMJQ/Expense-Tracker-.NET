e91875ac09c2984500909c5cd51e6b3278965126
Jackie <Jackie010227@outlook.com>
2024-04-16 22:10:55 -0230

    Category page finished!!

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -12,11 +12,20 @@ namespace Expense_Tracker.Models
 		[Column(TypeName = "nvarchar(50)")]
 		public string Title { get; set; }
 
-		[Column(TypeName = "nvarchar(5)")]
+		[Column(TypeName = "nvarchar(50)")]
 		public string Icon { get; set; } = "";
 
 		[Column(TypeName = "nvarchar(10)")]
-		public string Type { get; set; } = "Expense";
-	}
+		public string Type { get; set; } = "";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
+    }
 }
 
