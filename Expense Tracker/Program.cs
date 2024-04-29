8c2f2a7c08d1480be61f6289f8bc92cea9287668
Jackie <Jackie010227@outlook.com>
2024-04-29 01:35:48 -0230

    Dashboard part and layout finished!

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -29,7 +29,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
 
